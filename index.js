50a3246035f111fc7086b7faf541c4ab6fa1cb4a
beso <beskapoladishvili@gmail.com>
2022-04-30 17:54:06 +0400

    comments added

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const form = document.getElementById('form')
 const todosUL = document.getElementById('todos')
 const input = document.getElementById('input')
 
-
+//Extracting todoes from local storage
 const todosFromLoacalStorage = JSON.parse(localStorage.getItem('todos'))
-console.log(todosFromLoacalStorage)
 
+
+//if there is todos in LS we adde them from LS
 if (todosFromLoacalStorage) {
   todosFromLoacalStorage.forEach(todo => addTodo(todo))
 }
@@ -15,12 +16,15 @@ form.addEventListener('submit', (e) => {
   addTodo()
 })
 
+
+
 function addTodo(todo) {
   let todoText = input.value
+  //todo is from local storage if it is not empty we assign todoText from local storage
   if (todo) {
     todoText = todo.text
   }
-
+  //if there is a todoText, we create and prepare the li element to be append to the todosUL element
   if (todoText) {
     const todoEl = document.createElement('li')
     todoEl.innerText = todoText
@@ -46,9 +50,11 @@ function addTodo(todo) {
   }
 }
 
+// each action we invoke this function to be added todos data in local storage
 function updateLS() {
   const todosForLocalStorage = []
   const todosEl = document.querySelectorAll('li')
+
   todosEl.forEach(todoEl => {
     todosForLocalStorage.push({
       text: todoEl.innerHTML,
